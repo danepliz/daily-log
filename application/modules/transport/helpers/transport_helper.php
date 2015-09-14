951113dfe978389e1043bbd6bd29a00a17ce880b
danepliz <danepliz@gmail.com>
2015-09-14 08:05:23 +0545

    file removed

--- application/modules/transport/helpers/transport_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use transport\models\Transport;
-
-function getTransportStatusSelectElement($name, $selected = Transport::TRANSPORT_STATUS_ACTIVE, $attributes){
-    $statusArray = Transport::$transport_status;
-    echo form_dropdown($name, $statusArray, $selected, $attributes);
-}
